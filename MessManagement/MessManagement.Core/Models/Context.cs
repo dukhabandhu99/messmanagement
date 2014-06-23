29f6d55ce896a18ff63f9afb32844a2e42787a4c
Dukhabandhu Sahoo <dukhabandhus@gmail.com>
2014-06-23 23:20:39 +0530

    Added all MessManagement project files
    First commit...

---
+++ MessManagement/MessManagement.Core/Models/Context.cs
@@ -0,0 +1,12 @@
+﻿using System.Data.Entity;
+
+namespace MessManagement.Core.Models
+{
+    class Context : DbContext
+    {
+        public Context() : base("MesssManagement")
+        {
+            
+        }
+    }
+}
